228adf8bba916417612b26053ea6b45449ee9986
AshhadAzmi <80192389+AshhadAzmi@users.noreply.github.com>
2024-02-14 02:01:01 +0530

    7. ErrorHandling

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,5 @@
-using System.Text;
 using API.Extensions;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,9 +11,13 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
+
+
 // Configure the HTTP request pipeline.
 
 // app.UseHttpsRedirection();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("https://localhost:4200"));
