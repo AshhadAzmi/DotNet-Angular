b1e705bb13c9d5bc7e2f42c84122f3d4b825f473
AshhadAzmi <80192389+AshhadAzmi@users.noreply.github.com>
2024-02-21 22:55:10 +0530

    16. Authorization Added

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { MembersListComponent } from './members/members-list/members-list.compon
 import { MembersDetailComponent } from './members/members-detail/members-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { AuthGuard } from './_gaurds/Auth.guard';
+import { AuthGuard } from './_guards/Auth.guard';
 import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { NotFoundComponent } from './errors/no-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
-import { preventUnsavedChangesGuard } from './_gaurds/prevent-unsaved-changes.guard';
+import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 import { memberDetailedResolver } from './_resolvers/member-detailed.resolver';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { adminGuard } from './_guards/admin.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -26,6 +28,7 @@ const routes: Routes = [
       { path: 'member/edit', component: MemberEditComponent, canDeactivate: [preventUnsavedChangesGuard]},
       { path: 'lists', component: ListsComponent },
       { path: 'messages', component: MessagesComponent },
+      { path: 'admin', component: AdminPanelComponent, canActivate: [adminGuard] },
     ]
   },
   {path: 'errors', component: TestErrorComponent},
